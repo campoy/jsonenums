dfe6af903e8ad07a358e204c7d06ccd5e1eef352
Francesc Campoy <campoy@golang.org>
2015-02-04 13:38:28 +0000

    Improved generated code by defining map outside of function.

--- template.go
+++ template.go
@@ -20,30 +20,37 @@ import (
 
 {{range $typename, $values := .TypesAndValues}}
 
-func (r {{$typename}}) MarshalJSON() ([]byte, error) {
-    if s, ok := interface{}(r).(fmt.Stringer); ok {
-        return json.Marshal(s.String())
+var (
+    _{{$typename}}NameToValue = map[string]{{$typename}} {
+        {{range $values}}"{{.}}": {{.}},
+        {{end}}
     }
-    s, ok := map[{{$typename}}]string {
-        {{range $values}}{{.}}: "{{.}}",{{end}}
-    }[r]
-    if !ok {
-        return nil, fmt.Errorf("invalid {{$typename}}: %d", r)
-    }
-    return json.Marshal(s)
-}
 
-var _{{$typename}}NameToValue = map[string]{{$typename}} {
-    {{range $values}}"{{.}}": {{.}},{{end}}
-}
+    _{{$typename}}ValueToName = map[{{$typename}}]string {
+        {{range $values}}{{.}}: "{{.}}",
+        {{end}}
+    }
+)
 
 func init() {
     var v {{$typename}}
     if _, ok := interface{}(v).(fmt.Stringer); ok {
         _{{$typename}}NameToValue = map[string]{{$typename}} {
-            {{range $values}}interface{}({{.}}).(fmt.Stringer).String(): {{.}},{{end}}
+            {{range $values}}interface{}({{.}}).(fmt.Stringer).String(): {{.}},
+            {{end}}
         }
     }
+}
+
+func (r {{$typename}}) MarshalJSON() ([]byte, error) {
+    if s, ok := interface{}(r).(fmt.Stringer); ok {
+        return json.Marshal(s.String())
+    }
+    s, ok := _{{$typename}}ValueToName[r]
+    if !ok {
+        return nil, fmt.Errorf("invalid {{$typename}}: %d", r)
+    }
+    return json.Marshal(s)
 }
 
 func (r *{{$typename}}) UnmarshalJSON(data []byte) error {
