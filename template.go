3699e1253080aadb865de87257758cd742bd54bd
Daved <daved@codingmusica.com>
2015-04-23 09:30:11 -0700

    Added MarshalJSON and UnmarshalJSON comments to template to prevent golint from complaining.

--- template.go
+++ template.go
@@ -42,6 +42,7 @@ func init() {
     }
 }
 
+// MarshalJSON is generated.
 func (r {{$typename}}) MarshalJSON() ([]byte, error) {
     if s, ok := interface{}(r).(fmt.Stringer); ok {
         return json.Marshal(s.String())
@@ -53,6 +54,7 @@ func (r {{$typename}}) MarshalJSON() ([]byte, error) {
     return json.Marshal(s)
 }
 
+// UnmarshalJSON is generated.
 func (r *{{$typename}}) UnmarshalJSON(data []byte) error {
     var s string
     if err := json.Unmarshal(data, &s); err != nil {
